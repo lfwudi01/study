ff1f36ad930f35d142990e8c928469035232c466
master <470887793@qq.com>
2021-08-17 14:13:40 +0800

    master

--- jingdong/js/jingdong.js
+++ jingdong/js/jingdong.js
@@ -1,13 +1,8 @@
 window.onload=function(){
     /*
-        设置主页轮播图广告滚动
+        设置img_left_div的宽度随图片数量而变化
     */
-    var scrollImg = document.getElementById("scrollImg");
-    var imgArr=["img/scrImg/q.jpg","img/scrImg/q1.jpg","img/scrImg/q2.jpg","img/scrImg/q3.jpg","img/scrImg/q4.jpg","img/scrImg/q5.jpg","img/scrImg/q6.jpg","img/scrImg/q7.jpg","img/scrImg/q8.jpg"];
-    var index=0;
-    setInterval(function(){
-        index++;
-        index %= imgArr.length;
-        scrollImg.src=imgArr[index];
-    },3000);
+   var imgBox = document.getElementById("img_left_div");
+   var imgArr = imgBox.getElementsByTagName("img");
+   imgBox.style.width = 648*imgArr.length+"px";
 };
