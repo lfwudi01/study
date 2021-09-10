b7ec9c853810a8de0cb2b6c7a063e8911a78959e
master <470887793@qq.com>
2021-09-10 15:27:50 +0800

    master

--- jingdong/xiaomi/js/index.js
+++ jingdong/xiaomi/js/index.js
@@ -1,5 +1,41 @@
-window.onload=function(){
-    
+window.onload = function () {
+    /* 
+        右侧导航条事件
+    */
+    var ringhtnav = function () {
+        var seewd = document.body.offsetWidth;
+        if (seewd <= 1444) {
+            returntop.setAttribute('class', 'return-smail');
+        } else {
+            returntop.setAttribute('class', 'return-larger');
+        }
+        if (seewd <= 1280) {
+            returntop.style.marginRight = "0";
+            returntop.style.right = "0";
+        } else {
+            returntop.style.marginRight = "";
+            returntop.style.right = "";
+        }
+    }
+    var returntop = document.getElementById("returntop");
+    ringhtnav();
 
-    
+    window.onresize = function () {
+        ringhtnav();
+    }
+    var lasttop = document.getElementById("lasttop");
+    document.onscroll=function(){
+        var ht = document.documentElement.scrollTop;
+        console.log(ht);
+        if(ht>=700){
+            lasttop.style.display="block";
+        }else{
+            lasttop.style.display="";
+        }
+    }
+
+    lasttop.onclick=function(){
+        document.documentElement.scrollTop="0";
+    }
+
 }
