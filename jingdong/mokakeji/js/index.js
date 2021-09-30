baa6df6033e0db110c58ad5d1d5d55ac224d8131
master <470887793@qq.com>
2021-09-30 17:23:41 +0800

    master

--- jingdong/mokakeji/js/index.js
+++ jingdong/mokakeji/js/index.js
@@ -1,3 +1,53 @@
 window.onload=function(){
+    var righttitle = document.getElementById("right-title");
+    var as = righttitle.getElementsByTagName("a");
+    var rightcase=document.getElementsByClassName("rightcase-swarp");
+    var index=0;
+    function swiperadd(){
+        var swiper = new Swiper('.swiper-container', {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            autoplay: true,//等同于以下设置
+            autoplay: {
+                delay: 3000,
+                stopOnLastSlide: false,
+                disableOnInteraction: false,
+            },
+        });
+    }
+
+    for(var i=0;i<as.length;i++){
+        as[i].num=i;
+        as[i].onclick=function(){
+            index=this.num;
+            for(var a=0;a<as.length;a++){
+                as[a].style.backgroundColor="";
+                as[a].style.color="";
+            }
+            this.style.backgroundColor="#007BFF";
+            this.style.color="#fff";
+            for(var b=0;b<rightcase.length;b++){
+                rightcase[b].style.display="none";
+                
+            }
+            setTimeout(function(){
+                rightcase[index].style.display="block";
+                swiperadd();
+            },200);
+            
+            
+            
+        }
+    }
+
     
 }
